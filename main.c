5de2cc462298cd0867d9a982b63714f3f82d4fcf
Fabien DEL CARMEN <fdel-car@e1r6p3.42.fr>
2016-10-19 19:12:13 +0200

    sliders

--- main.c
+++ main.c
@@ -26,26 +26,22 @@ void	put_pixel(t_gtk * gtk, int x, int y, guchar red, guchar green,
 void	raytracing(void)
 {
 	int x, y = 0;
-	float old_time, ftime;
 
-	gtk->time = clock();
+	gtk->red = gtk_range_get_value(GTK_RANGE(gtk->red_scale));
+	gtk->green = gtk_range_get_value(GTK_RANGE(gtk->green_scale));
+	gtk->blue = gtk_range_get_value(GTK_RANGE(gtk->blue_scale));
 	x = 0;
 	while (x < WIDTH)
 	{
 		y = 0;
 		while (y < HEIGHT)
 		{
-			put_pixel(gtk, x, y, 100, 100, 100);
+			put_pixel(gtk, x, y, gtk->red, gtk->green, gtk->blue);
 			y++;
 		}
 		x++;
 	}
 	gtk_image_set_from_pixbuf(GTK_IMAGE(gtk->img), gtk->pix);
-	old_time = gtk->time;
-	gtk->time = clock();
-	ftime = (gtk->time - old_time) / CLOCKS_PER_SEC;
-	ft_putnbr(1.0 / ftime);
-	ft_putchar('\n');
 }
 
 int		main(int argc, char **argv)
@@ -54,7 +50,6 @@ int		main(int argc, char **argv)
     gtk_init(&argc, &argv);
     gtk->win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(gtk->win), "Raytracer");
-	gtk_window_set_default_size(GTK_WINDOW(gtk->win), WIDTH, HEIGHT);
 	gtk_window_set_position(GTK_WINDOW(gtk->win), GTK_WIN_POS_CENTER);
 	g_signal_connect(G_OBJECT(gtk->win), "delete-event",
 	G_CALLBACK(gtk_main_quit), NULL);
@@ -62,14 +57,50 @@ int		main(int argc, char **argv)
 	G_CALLBACK(raytracing), NULL);
 	gtk->pix = gdk_pixbuf_new(GDK_COLORSPACE_RGB, FALSE, 8, WIDTH, HEIGHT);
 	gtk->img = gtk_image_new_from_pixbuf(gtk->pix);
-	gtk->grid = gtk_grid_new();
+	gtk->box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
 	gtk->list = gtk_list_box_new();
-	gtk_grid_attach(GTK_GRID(gtk->grid), gtk->list, 0, 0, 1, 1);
-	gtk_grid_attach_next_to(GTK_GRID(gtk->grid), gtk->img, gtk->list,
-	GTK_POS_RIGHT, 1, 1);
 
-	gtk_container_add(GTK_CONTAINER(gtk->win), gtk->grid);
+	// Sliders
+	gtk->red_adjust = gtk_adjustment_new(125.0, 0.0, 255.0, 1.0, 51.0, 0.0);
+	gtk->green_adjust = gtk_adjustment_new(125.0, 0.0, 255.0, 1.0, 51.0, 0.0);
+	gtk->blue_adjust = gtk_adjustment_new(125.0, 0.0, 255.0, 1.0, 51.0, 0.0);
+	gtk->red_scale = gtk_scale_new(GTK_ORIENTATION_HORIZONTAL, gtk->red_adjust);
+	gtk->green_scale = gtk_scale_new(GTK_ORIENTATION_HORIZONTAL, gtk->green_adjust);
+	gtk->blue_scale = gtk_scale_new(GTK_ORIENTATION_HORIZONTAL, gtk->blue_adjust);
+	gtk_scale_set_digits(GTK_SCALE(gtk->red_scale), 0);
+	gtk_scale_set_digits(GTK_SCALE(gtk->green_scale), 0);
+	gtk_scale_set_digits(GTK_SCALE(gtk->blue_scale), 0);
+	gtk_widget_set_size_request(gtk->red_scale, 200, 10);
+	gtk_widget_set_size_request(gtk->green_scale, 200, 10);
+	gtk_widget_set_size_request(gtk->blue_scale, 200, 10);
+	gtk->button = gtk_button_new_with_label("Draw image");
+	g_signal_connect(G_OBJECT(gtk->button), "button-press-event",
+	G_CALLBACK(raytracing), NULL);
 
+	// List insertion
+	gtk->box_red = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+	gtk_box_pack_start(GTK_BOX(gtk->box_red), gtk_label_new("Red :"),
+	FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(gtk->box_red), gtk->red_scale, FALSE, FALSE, 0);
+	gtk->box_green = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+	gtk_box_pack_start(GTK_BOX(gtk->box_green), gtk_label_new("Green :"),
+	FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(gtk->box_green), gtk->green_scale,
+	FALSE, FALSE, 0);
+	gtk->box_blue = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+	gtk_box_pack_start(GTK_BOX(gtk->box_blue), gtk_label_new("Blue :"),
+	FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(gtk->box_blue), gtk->blue_scale,
+	FALSE, FALSE, 0);
+
+	gtk_list_box_insert(GTK_LIST_BOX(gtk->list), gtk->box_red, -1);
+	gtk_list_box_insert(GTK_LIST_BOX(gtk->list), gtk->box_green, -1);
+	gtk_list_box_insert(GTK_LIST_BOX(gtk->list), gtk->box_blue, -1);
+	gtk_list_box_insert(GTK_LIST_BOX(gtk->list), gtk->button, -1);
+
+	gtk_box_pack_start(GTK_BOX(gtk->box), gtk->list, FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(gtk->box), gtk->img, FALSE, FALSE, 0);
+	gtk_container_add(GTK_CONTAINER(gtk->win), gtk->box);
 	gtk->pixels = gdk_pixbuf_get_pixels(gtk->pix);
 	gtk->rowstride = WIDTH * 3;
 	raytracing();
--- rt.h
+++ rt.h
@@ -12,29 +12,40 @@
 
 #ifndef RT_H
 # define RT_H
-# define WIDTH 720
-# define HEIGHT 480
+# define WIDTH 1920
+# define HEIGHT 1080
 # define SQ(x) ((x) * (x))
 
 #include <stdlib.h>
 #include <gtk/gtk.h>
 #include <stdlib.h>
-#include <time.h>
 #include <math.h>
 #include "libft.h"
 
 typedef struct	s_gtk
 {
-	GtkWidget		*win;
 	GdkPixbuf		*pix;
+	GtkAdjustment	*red_adjust;
+	GtkAdjustment	*green_adjust;
+	GtkAdjustment	*blue_adjust;
+	GtkWidget		*win;
 	GtkWidget		*img;
-	GtkWidget		*grid;
+	GtkWidget		*box;
+	GtkWidget		*box_red;
+	GtkWidget		*box_green;
+	GtkWidget		*box_blue;
 	GtkWidget		*list;
+	GtkWidget		*red_scale;
+	GtkWidget		*green_scale;
+	GtkWidget		*blue_scale;
+	GtkWidget		*button;
 	GMainLoop		*loop;
 	guchar			*pixels;
+	guchar			red;
+	guchar			green;
+	guchar			blue;
 	int				rowstride;
 	unsigned int	pos;
-	float			time;
 }					t_gtk;
 
 t_gtk *gtk;
