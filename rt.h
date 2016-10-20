26abc80ffe21ab89a0aa755c28e2e4e186b8e876
Fabien DEL CARMEN <fdel-car@e1r5p3.42.fr>
2016-10-20 16:01:25 +0200

    Structure gloable

--- rt.h
+++ rt.h
@@ -16,13 +16,75 @@
 # define HEIGHT 1080
 # define SQ(x) ((x) * (x))
 
-#include <stdlib.h>
-#include <gtk/gtk.h>
-#include <stdlib.h>
-#include <math.h>
-#include "libft.h"
+# include <stdlib.h>
+# include <gtk/gtk.h>
+# include <stdlib.h>
+# include <math.h>
+# include "libft.h"
 
-typedef struct	s_gtk
+typedef struct		s_vect
+{
+	float			x;
+	float			y;
+	float			z;
+}					t_vect;
+
+typedef struct		s_color
+{
+	unsigned char	r;
+	unsigned char	g;
+	unsigned char	b;
+}					t_color;
+
+typedef struct		s_cam
+{
+	t_vect			pos;
+	t_vect			dir;
+}					t_cam;
+
+typedef struct		s_lights
+{
+	gboolean		blur;
+	t_vect			pos;
+	t_color			color;
+	float			rayon;
+	float			intensity;
+	char			*type;
+	void			*func;
+}					t_lights;
+
+typedef struct		s_mater
+{
+	gboolean		shiny;
+	float			int_refl;
+	float			int_trans;
+	t_color			color;
+	char			*text;
+	void			*func;
+}					t_mater;
+
+typedef struct		s_obj
+{
+	char			*name;
+	t_vect			pos;
+	t_vect			dir;
+	t_vect			norm;
+	float			rayon;
+	t_mater			mater;
+	void			*next;
+}					t_obj;
+
+typedef struct		s_scene
+{
+	gboolean		gl_illum;
+	int				iter_refl;
+	char			*name;
+	t_cam			cam;
+	t_lights		*lst_lght;
+	t_obj			*lst_obj;
+}					t_scene;
+
+typedef struct		s_env
 {
 	GdkPixbuf		*pix;
 	GtkAdjustment	*red_adjust;
@@ -41,13 +103,11 @@ typedef struct	s_gtk
 	GtkWidget		*button;
 	GMainLoop		*loop;
 	guchar			*pixels;
-	guchar			red;
-	guchar			green;
-	guchar			blue;
-	int				rowstride;
-	unsigned int	pos;
-}					t_gtk;
+	gint			rowstride;
+	t_color			color;
+	t_scene			scene;
+}					t_env;
 
-t_gtk *gtk;
+t_env g_env;
 
 #endif
