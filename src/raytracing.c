27839e8ed26288b62ea5fe2ff61be44652c29587
Victor DE LA SALMONIERE <vde-la-s@e1r8p3.42.fr>
2016-10-26 16:20:07 +0200

    dq

--- src/raytracing.c
+++ src/raytracing.c
@@ -6,7 +6,7 @@
 /*   By: vde-la-s <vde-la-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/24 17:53:11 by fdel-car          #+#    #+#             */
-/*   Updated: 2016/10/26 16:19:05 by vde-la-s         ###   ########.fr       */
+/*   Updated: 2016/10/26 16:20:03 by vde-la-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,12 +99,10 @@ t_color	compute_light(t_data ray)
 	t_color c;
 	t_light *l;
 	gboolean sh;
-	int				i;
 
-	i = 0;
 	l = g_env.scene.lgt;
 	c = color_mult(ray.obj_hit->mater.color, 0.2);
-	while (l && ++i)
+	while (l)
 	{
 		sh = is_shadowed(l, &ray);
 		if (sh != FALSE)
