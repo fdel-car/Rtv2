a3c12dc4afe1c87d2df0e8155676e4378d4a0d88
Victor DE LA SALMONIERE <vde-la-s@e1r8p10.42.fr>
2016-11-07 16:32:15 +0100

    updated mesh

--- src/parse.obj.c
+++ src/parse.obj.c
@@ -30,25 +30,26 @@ int get_val(char *s, int r)
 	return (ft_atoi(&(s[i])));
 }
 
-void read_triangle(t_obj *o, char *line, t_vect *v, t_vect *vn, t_mater mat)
+void read_triangle(t_obj *o, char *line, t_vect **v, t_mater mat)
 {
 	char **t;
 
 	t = ft_strsplit(line, ' ');
 	if (t[0] && t[1] && t[2] && t[3])
 	{
+			o->name = ft_strdup("mesh_triangle");
 			o->type = TRIANGLE;
-			o->norm = vn[get_val(t[1], 3) - 1];
-			o->v0 = v[get_val(t[1], 1) - 1];
-			o->v1 = v[get_val(t[2], 1) - 1];
-			o->v2 = v[get_val(t[3], 1) - 1];
+			o->norm = v[1][get_val(t[1], 3) - 1];
+			o->v0 = v[0][get_val(t[1], 1) - 1];
+			o->v1 = v[0][get_val(t[2], 1) - 1];
+			o->v2 = v[0][get_val(t[3], 1) - 1];
 			o->mater = mat;
 			o->next = 0;
 	}
 	ctab_free(t);
 }
 
-void load_obj(char **file, t_vect *v, t_vect *vn, int i, t_mater mat)
+void load_obj(char **file, t_vect **v, int i, t_mater mat)
 {
 	t_obj *new;
 
@@ -57,7 +58,7 @@ void load_obj(char **file, t_vect *v, t_vect *vn, int i, t_mater mat)
 	while (file[i])
 	{
 		new = malloc(sizeof(t_obj));
-		read_triangle(new, file[i], v, vn, mat);
+		read_triangle(new, file[i], v, mat);
 		set_func(new);
 		push_obj(new);
 		++i;
@@ -66,25 +67,24 @@ void load_obj(char **file, t_vect *v, t_vect *vn, int i, t_mater mat)
 
 void fill_values(char **file, int v_s, int vn_s, t_mater mat)
 {
-	t_vect *v;
-	t_vect *vn;
+	t_vect *v[2];
 	int i;
 	int in;
 
 	i = 0;
-	v = malloc(sizeof(t_vect) * (v_s + 1));
-	vn = malloc(sizeof(t_vect) * (vn_s + 1));
+	v[0] = malloc(sizeof(t_vect) * (v_s + 1));
+	v[1] = malloc(sizeof(t_vect) * (vn_s + 1));
 	while (file[i] && !ft_sii(file[i], "v "))
 		++i;
 	in = 0;
 	while (file[i] && ft_sii(file[i], "v "))
-			v[in++] = read_vec(get_after(file[i++], "v "), ' ');
+			v[0][in++] = read_vec(get_after(file[i++], "v "), ' ');
 	while (file[i] && !ft_sii(file[i], "vn "))
 		++i;
 	in = 0;
 	while (file[i] && ft_sii(file[i], "vn "))
-			vn[in++] = read_vec(get_after(file[i++], "vn "), ' ');
-	load_obj(file, v, vn, i, mat);
+			v[1][in++] = read_vec(get_after(file[i++], "vn "), ' ');
+	load_obj(file, v, i, mat);
 }
 
 void parse_obj(char *file_name, t_mater mat)
