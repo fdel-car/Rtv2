4249168e8f66e47108ce8f0d3aca757b62133eb5
Fabien DEL CARMEN <fdel-car@e1r8p5.42.fr>
2016-12-10 20:18:52 +0100

    adn dir not pos yet

--- src/complex_objects.c
+++ src/complex_objects.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   complex_objects.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vde-la-s <vde-la-s@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fdel-car <fdel-car@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/29 22:20:34 by fdel-car          #+#    #+#             */
-/*   Updated: 2016/12/10 17:18:36 by vde-la-s         ###   ########.fr       */
+/*   Updated: 2016/12/10 20:18:23 by fdel-car         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 void	add_sphere(t_obj *o, t_vect pos)
 {
-	t_obj *sp;
+	t_obj	*sp;
+
 	sp = malloc(sizeof(t_obj));
 	init_obj(sp);
 	sp->next = NULL;
@@ -41,8 +42,9 @@ void	add_sphere(t_obj *o, t_vect pos)
 void	add_torus_sphere(t_obj *o)
 {
 	t_vect	pos;
-	int		i = 0;
+	int		i;
 
+	i = 0;
 	pos = vec_new(o->pos.x + o->rayon, o->pos.y, o->pos.z);
 	while (i < 360)
 	{
@@ -79,9 +81,9 @@ void	add_sphere_adn(t_obj *o, t_vect pos)
 	push_to_obj(sp, o);
 }
 
-void	add_cylindre_adn(t_obj *o, t_vect pos, t_vect pos1)
+void	add_cylindre_adn(t_obj *o, t_vect pos, t_vect pos2)
 {
-	t_obj *sp;
+	t_obj	*sp;
 
 	sp = malloc(sizeof(t_obj));
 	init_obj(sp);
@@ -89,16 +91,17 @@ void	add_cylindre_adn(t_obj *o, t_vect pos, t_vect pos1)
 	sp->src = NULL;
 	sp->name = "tic";
 	sp->cut = vec_new(0, 0, 0);
-	sp->min = - (o->rayon);
+	sp->min = -(o->rayon);
 	sp->max = o->rayon;
-	sp->pos = vec_new(pos.x, pos.y,pos.z);
-	sp->dir = vec_norm(vec_sub(pos, pos1));
+	sp->pos = pos;
+	sp->dir = vec_norm(vec_sub(pos, pos2));
 	sp->rayon = 0.12;
 	sp->type = CYLINDER;
 	sp->mater.shiny = o->mater.shiny;
 	sp->mater.int_refl = o->mater.int_refl;
 	sp->mater.int_trans = o->mater.int_trans;
-	sp->mater.color = color_new(o->mater.color.g,o->mater.color.b,o->mater.color.r);
+	sp->mater.color = color_new(o->mater.color.g, o->mater.color.b,
+	o->mater.color.r);
 	sp->mater.tex = o->mater.tex;
 	sp->mater.ntex = o->mater.ntex;
 	sp->mater.indice = o->mater.indice;
@@ -106,24 +109,27 @@ void	add_cylindre_adn(t_obj *o, t_vect pos, t_vect pos1)
 	sp->func = &intersect_cylinder;
 	push_to_obj(sp, o);
 }
+
 void	add_adn(t_obj *o)
 {
 	t_vect	pos;
-	t_vect	pos1;
-	int		i = 0;
+	t_vect	pos2;
+	int		i;
 
-
-	pos = vec_new(o->pos.x + o->rayon, o->pos.y, o->pos.z);
-	pos1 = vec_new(o->pos.x - o->rayon, o->pos.y, o->pos.z);
+	i = 0;
+	if (vec_dotp(o->dir, o->norm) != 0)
+		return ;
+	pos = vec_mult(o->norm, o->rayon);
+	pos2 = vec_mult(o->norm, -o->rayon);
 	while (i < 18)
 	{
-		pos.y += 0.8;
-		pos1.y += 0.8;
+		pos = vec_add(pos, vec_mult(o->dir, 0.8));
+		pos2 = vec_add(pos2, vec_mult(o->dir, 0.8));
 		add_sphere_adn(o, pos);
-		add_sphere_adn(o, pos1);
-		add_cylindre_adn(o,pos, pos1);
-		pos = rot_y(20, pos);
-		pos1 = rot_y(20, pos1);
+		add_sphere_adn(o, pos2);
+		add_cylindre_adn(o, pos, pos2);
+		pos = rot_any(pos, o->dir, 20);
+		pos2 = rot_any(pos2, o->dir, 20);
 		i++;
 	}
 }
