04cdf63a699550093eb1c3aa57b6eb8b2f895138
Fabien DEL CARMEN <fdel-car@e1r12p6.42.fr>
2016-12-13 19:22:44 +0100

    Norme Fab

--- src/gtk/gtk_f5.c
+++ src/gtk/gtk_f5.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gtk_f5.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdel-car <fdel-car@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/13 19:16:20 by fdel-car          #+#    #+#             */
+/*   Updated: 2016/12/13 19:18:08 by fdel-car         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "rt.h"
 
 t_obj	*add_object_mater_refl(t_obj *obj)
 {
 	GtkEntry	*refl;
 
-	refl = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_refl"));
+	refl = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_refl"));
 	obj->mater.int_refl = ft_atof(gtk_entry_get_text(refl));
 	return (obj);
 }
@@ -13,7 +25,7 @@ t_obj	*add_object_mater_trans(t_obj *obj)
 {
 	GtkEntry	*trans;
 
-	trans = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_trans"));
+	trans = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_trans"));
 	obj->mater.int_trans = ft_atof(gtk_entry_get_text(trans));
 	return (obj);
 }
@@ -24,8 +36,8 @@ t_obj	*add_object_mater_color(t_obj *obj)
 	GtkColorChooser	*e;
 
 	col = malloc(sizeof(GdkRGBA));
-	e = GTK_COLOR_CHOOSER(gtk_builder_get_object(g_env.build,"e_color"));
-	gtk_color_chooser_get_rgba (e,col);
+	e = GTK_COLOR_CHOOSER(gtk_builder_get_object(g_env.build, "e_color"));
+	gtk_color_chooser_get_rgba(e, col);
 	obj->mater.color.r = (int)(col->red * 255);
 	obj->mater.color.g = (int)(col->green * 255);
 	obj->mater.color.b = (int)(col->blue * 255);
@@ -40,14 +52,14 @@ void	clear_entry_1(void)
 	GtkEntry	*posy;
 	GtkEntry	*posz;
 
-	name = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_name"));
-	posx = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_posx"));
-	posy = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_posy"));
-	posz = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_posz"));
-	gtk_entry_set_text(name,"");
-	gtk_entry_set_text(posx,"");
-	gtk_entry_set_text(posy,"");
-	gtk_entry_set_text(posz,"");
+	name = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_name"));
+	posx = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_posx"));
+	posy = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_posy"));
+	posz = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_posz"));
+	gtk_entry_set_text(name, "");
+	gtk_entry_set_text(posx, "");
+	gtk_entry_set_text(posy, "");
+	gtk_entry_set_text(posz, "");
 }
 
 void	clear_entry_2(void)
@@ -57,12 +69,12 @@ void	clear_entry_2(void)
 	GtkEntry	*dirz;
 	GtkEntry	*rayon;
 
-	rayon = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_rayon"));
-	dirx = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_dirx"));
-	diry = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_diry"));
-	dirz = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_dirz"));
-	gtk_entry_set_text(rayon,"");
-	gtk_entry_set_text(dirx,"");
-	gtk_entry_set_text(diry,"");
-	gtk_entry_set_text(dirz,"");
+	rayon = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_rayon"));
+	dirx = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_dirx"));
+	diry = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_diry"));
+	dirz = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_dirz"));
+	gtk_entry_set_text(rayon, "");
+	gtk_entry_set_text(dirx, "");
+	gtk_entry_set_text(diry, "");
+	gtk_entry_set_text(dirz, "");
 }
