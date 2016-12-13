f6bba4ad2224235e870329ce8547350f6737e5ea
Fabien DEL CARMEN <fdel-car@e1r12p6.42.fr>
2016-12-13 19:15:14 +0100

    Norme Fab

--- src/gtk/gtk_f4.c
+++ src/gtk/gtk_f4.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gtk_f4.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdel-car <fdel-car@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/13 19:14:57 by fdel-car          #+#    #+#             */
+/*   Updated: 2016/12/13 19:14:58 by fdel-car         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "rt.h"
 
 t_obj	*add_object_dir(t_obj *obj)
@@ -6,9 +18,9 @@ t_obj	*add_object_dir(t_obj *obj)
 	GtkEntry	*diry;
 	GtkEntry	*dirz;
 
-	dirx = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_dirx"));
-	diry = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_diry"));
-	dirz = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_dirz"));
+	dirx = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_dirx"));
+	diry = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_diry"));
+	dirz = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_dirz"));
 	obj->dir.x = ft_atof(gtk_entry_get_text(dirx));
 	obj->dir.y = ft_atof(gtk_entry_get_text(diry));
 	obj->dir.z = ft_atof(gtk_entry_get_text(dirz));
@@ -22,9 +34,9 @@ t_obj	*add_object_norm(t_obj *obj)
 	GtkEntry	*normy;
 	GtkEntry	*normz;
 
-	normx = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_normx"));
-	normy = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_normy"));
-	normz = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_normz"));
+	normx = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_normx"));
+	normy = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_normy"));
+	normz = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_normz"));
 	obj->norm.x = ft_atof(gtk_entry_get_text(normx));
 	obj->norm.y = ft_atof(gtk_entry_get_text(normy));
 	obj->norm.z = ft_atof(gtk_entry_get_text(normz));
@@ -36,7 +48,7 @@ t_obj	*add_object_rayon(t_obj *obj)
 {
 	GtkEntry	*rayon;
 
-	rayon = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_rayon"));
+	rayon = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_rayon"));
 	obj->rayon = ft_atof(gtk_entry_get_text(rayon));
 	return (obj);
 }
@@ -45,7 +57,7 @@ t_obj	*add_object_alpha(t_obj *obj)
 {
 	GtkEntry	*alpha;
 
-	alpha = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_angle"));
+	alpha = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_angle"));
 	obj->alpha = ft_atof(gtk_entry_get_text(alpha)) * M_PI / 180;
 	return (obj);
 }
@@ -54,7 +66,7 @@ t_obj	*add_object_mater_shiny(t_obj *obj)
 {
 	GtkEntry	*shiny;
 
-	shiny = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_shiny"));
+	shiny = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_shiny"));
 	obj->mater.shiny = ft_atof(gtk_entry_get_text(shiny));
 	return (obj);
 }
