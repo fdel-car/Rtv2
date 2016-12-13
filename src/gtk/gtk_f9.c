75885f87ca14502033786d784aff2f1c94c09ad4
victor <vic@MacBook-Pro-de-victor.local>
2016-12-13 19:29:11 +0100

    gtk_f9 norme

--- src/gtk/gtk_f9.c
+++ src/gtk/gtk_f9.c
@@ -1,61 +1,62 @@
 #include "rt.h"
 
+void	save_translation_mesh_object2(t_obj *list, t_gtkdata *data, t_vect oldpos)
+{
+	if (ft_strcmp(data->desc,"posx") == 0)
+	{
+		list->v0.x  += (((t_obj*)data->obj)->pos.x - oldpos.x);
+		list->v1.x  += (((t_obj*)data->obj)->pos.x - oldpos.x);
+		list->v2.x  += (((t_obj*)data->obj)->pos.x - oldpos.x);
+	}
+	else if (ft_strcmp(data->desc,"posy") == 0)
+	{
+		list->v0.y  += (((t_obj*)data->obj)->pos.y - oldpos.y);
+		list->v1.y  += (((t_obj*)data->obj)->pos.y - oldpos.y);
+		list->v2.y  += (((t_obj*)data->obj)->pos.y - oldpos.y);
+	}
+	else if (ft_strcmp(data->desc,"posz") == 0)
+	{
+		list->v0.z += (((t_obj*)data->obj)->pos.z - oldpos.z);
+		list->v1.z += (((t_obj*)data->obj)->pos.z - oldpos.z);
+		list->v2.z += (((t_obj*)data->obj)->pos.z - oldpos.z);
+	}
+}
+
+void	save_translation_mesh_object3(t_obj *list, t_gtkdata *data, t_vect oldpos)
+{
+	if(ft_strcmp(data->desc,"posx") == 0)
+	{
+		list->pos.x  += (((t_obj *)data->obj)->pos.x - oldpos.x);
+	}
+	else if(ft_strcmp(data->desc,"posy") == 0)
+	{
+		list->pos.y  += (((t_obj *)data->obj)->pos.y - oldpos.y);
+	}
+	else if(ft_strcmp(data->desc,"posz") == 0)
+	{
+		list->pos.z += (((t_obj *)data->obj)->pos.z - oldpos.z);
+	}
+}
+
 void	save_translation_mesh_object(GtkEntry *entry, t_gtkdata *data, t_vect oldpos)
 {
-	t_obj *list;
-	float ret;
+	t_obj	*list;
+	float	ret;
 
 	list = ((t_obj *)data->obj)->lst;
 	ret = ft_atof(gtk_entry_get_text(entry));
 	while (list)
 	{
 		if(list->type == TRIANGLE)
-		{
-			if(ft_strcmp(data->desc,"posx") == 0)
-			{
-				list->v0.x  += (((t_obj *)data->obj)->pos.x - oldpos.x);
-				list->v1.x  += (((t_obj *)data->obj)->pos.x - oldpos.x);
-				list->v2.x  += (((t_obj *)data->obj)->pos.x - oldpos.x);
-			}
-			else if(ft_strcmp(data->desc,"posy") == 0)
-			{
-				list->v0.y  += (((t_obj *)data->obj)->pos.y - oldpos.y);
-				list->v1.y  += (((t_obj *)data->obj)->pos.y - oldpos.y);
-				list->v2.y  += (((t_obj *)data->obj)->pos.y - oldpos.y);
-			}
-			else if(ft_strcmp(data->desc,"posz") == 0)
-			{
-				list->v0.z += (((t_obj *)data->obj)->pos.z - oldpos.z);
-				list->v1.z += (((t_obj *)data->obj)->pos.z - oldpos.z);
-				list->v2.z += (((t_obj *)data->obj)->pos.z - oldpos.z);
-			}
-		}
+			save_translation_mesh_object2(list, data, oldpos);
 		else
-		{
-			if(ft_strcmp(data->desc,"posx") == 0)
-			{
-				list->pos.x  += (((t_obj *)data->obj)->pos.x - oldpos.x);
-			}
-			else if(ft_strcmp(data->desc,"posy") == 0)
-			{
-				list->pos.y  += (((t_obj *)data->obj)->pos.y - oldpos.y);
-			}
-			else if(ft_strcmp(data->desc,"posz") == 0)
-			{
-				list->pos.z += (((t_obj *)data->obj)->pos.z - oldpos.z);
-			}
-		}
+			save_translation_mesh_object3(list, data, oldpos);
 		list = list->next;
 	}
 }
 
-void	save_entry_transformation_object(GtkEntry *entry, t_gtkdata *data)
+void	save_entry_transformation_object2(t_gtkdata *data, float ret)
 {
-	float ret;
-	t_vect pos;
-
-	ret = ft_atof(gtk_entry_get_text(entry));
-	pos = ((t_obj *)data->obj)->pos;
 	if(ft_strcmp(data->desc,"posx") == 0)
 		((t_obj *)data->obj)->pos.x  = ret;
 	if(ft_strcmp(data->desc,"posy") == 0)
@@ -71,20 +72,26 @@ void	save_entry_transformation_object(GtkEntry *entry, t_gtkdata *data)
 	((t_obj *)data->obj)->dir = vec_norm(((t_obj *)data->obj)->dir);
 	if(ft_strcmp(data->desc,"normx") == 0)
 		((t_obj *)data->obj)->norm.x  = ret;
-	if(ft_strcmp(data->desc,"normy") == 0)
+}
+
+void	save_entry_transformation_object(GtkEntry *entry, t_gtkdata *data)
+{
+	float	ret;
+	t_vect	pos;
+
+	ret = ft_atof(gtk_entry_get_text(entry));
+	pos = ((t_obj *)data->obj)->pos;
+	save_entry_transformation_object2(data, ret);
+	if (ft_strcmp(data->desc,"normy") == 0)
 		((t_obj *)data->obj)->norm.y  = ret;
-	if(ft_strcmp(data->desc,"normz") == 0)
+	if (ft_strcmp(data->desc,"normz") == 0)
 		((t_obj *)data->obj)->norm.z = ret;
-	if(ft_strcmp(data->desc,"rayon") == 0)
+	if (ft_strcmp(data->desc,"rayon") == 0)
 		((t_obj *)data->obj)->rayon  = ret;
-	if(ft_strcmp(data->desc,"alpha") == 0)
+	if (ft_strcmp(data->desc,"alpha") == 0)
 		((t_obj *)data->obj)->alpha  = ret * M_PI / 180;
 	if (((t_obj *)data->obj)->type == MESH )
-	{
-		if(ft_strcmp(data->desc,"posx") == 0 || ft_strcmp(data->desc,"posy") ==
+		if (ft_strcmp(data->desc,"posx") == 0 || ft_strcmp(data->desc,"posy") ==
 				0 || ft_strcmp(data->desc,"posz") == 0)
-		{
 			save_translation_mesh_object(entry,data, pos);
-		}
-	}
 }
