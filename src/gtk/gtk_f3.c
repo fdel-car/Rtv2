8aa06738238c6cd8081c9c90efaa2e1fb613d78a
Fabien DEL CARMEN <fdel-car@e1r12p6.42.fr>
2016-12-13 19:14:15 +0100

    Norme Fab

--- src/gtk/gtk_f3.c
+++ src/gtk/gtk_f3.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gtk_f3.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdel-car <fdel-car@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/13 19:12:54 by fdel-car          #+#    #+#             */
+/*   Updated: 2016/12/13 19:12:55 by fdel-car         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "rt.h"
 
 void	init_gtk_filter_widget(void)
 {
 	GtkWidget	*combo;
-	static char	*combo_text[] = {"None","Sepia","Cartoon","Greyscale",
+	static char	*combo_text[] = {"None", "Sepia", "Cartoon", "Greyscale",
 	"Stereocopique", "Oculus"};
 	int			i;
 
 	i = 0;
-	combo = GTK_WIDGET(gtk_builder_get_object(g_env.build,"filtre_combo"));
+	combo = GTK_WIDGET(gtk_builder_get_object(g_env.build, "filtre_combo"));
 	while (i < 6)
 	{
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo),
@@ -23,11 +35,12 @@ void	init_gtk_filter_widget(void)
 void	init_gtk_create_widget(void)
 {
 	GtkWidget	*combo;
-	static char	*combo_text[] = {"Sphere", "Plan", "Cone", "Cylindre", "Torus", "ADN"};
+	static char	*combo_text[] = {"Sphere", "Plan", "Cone", "Cylindre",
+	"Torus", "ADN"};
 	int			i;
 
 	i = 0;
-	combo = GTK_WIDGET(gtk_builder_get_object(g_env.build,"create_type"));
+	combo = GTK_WIDGET(gtk_builder_get_object(g_env.build, "create_type"));
 	while (i < 6)
 	{
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo),
@@ -40,17 +53,17 @@ t_obj	*add_object_type(char *s, t_obj *obj)
 {
 	if (!s)
 		obj->type = NONE;
-	else if (ft_strcmp("Sphere",s) == 0)
+	else if (ft_strcmp("Sphere", s) == 0)
 		obj->type = SPHERE;
-	else if (ft_strcmp("Plan",s) == 0)
+	else if (ft_strcmp("Plan", s) == 0)
 		obj->type = PLANE;
-	else if (ft_strcmp("Cone",s) == 0)
+	else if (ft_strcmp("Cone", s) == 0)
 		obj->type = CONE;
-	else if (ft_strcmp("Cylindre",s) == 0)
+	else if (ft_strcmp("Cylindre", s) == 0)
 		obj->type = CYLINDER;
-	else if (ft_strcmp("Torus",s) == 0)
+	else if (ft_strcmp("Torus", s) == 0)
 		obj->type = MESH;
-	else if (ft_strcmp("ADN",s) == 0)
+	else if (ft_strcmp("ADN", s) == 0)
 		obj->type = MESH;
 	return (obj);
 }
@@ -59,7 +72,7 @@ t_obj	*add_object_name(t_obj *obj)
 {
 	GtkEntry	*name;
 
-	name = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_name"));
+	name = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_name"));
 	if (ft_strcmp("", gtk_entry_get_text(name)) == 0)
 		obj->name = ft_strdup("no name");
 	obj->name = ft_strdup(gtk_entry_get_text(name));
@@ -72,9 +85,9 @@ t_obj	*add_object_pos(t_obj *obj)
 	GtkEntry	*posy;
 	GtkEntry	*posz;
 
-	posx = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_posx"));
-	posy = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_posy"));
-	posz = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_posz"));
+	posx = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_posx"));
+	posy = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_posy"));
+	posz = GTK_ENTRY(gtk_builder_get_object(g_env.build, "e_posz"));
 	obj->pos.x = ft_atof(gtk_entry_get_text(posx));
 	obj->pos.y = ft_atof(gtk_entry_get_text(posy));
 	obj->pos.z = ft_atof(gtk_entry_get_text(posz));
