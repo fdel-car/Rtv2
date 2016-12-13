bd20d0d49775b2950a4e504c74570db5b9cd5e86
victor <vic@MacBook-Pro-de-victor.local>
2016-12-13 18:17:37 +0100

    gtk_f3 norme

--- src/gtk/gtk_f3.c
+++ src/gtk/gtk_f3.c
@@ -2,17 +2,17 @@
 
 void	init_gtk_filter_widget(void)
 {
-	GtkWidget *combo;
-	static char *combo_text[] = {"None","Sepia","Cartoon","Greyscale",
-		"Stereocopique", "Oculus"};
-	int		i;
+	GtkWidget	*combo;
+	static char	*combo_text[] = {"None","Sepia","Cartoon","Greyscale",
+	"Stereocopique", "Oculus"};
+	int			i;
 
 	i = 0;
 	combo = GTK_WIDGET(gtk_builder_get_object(g_env.build,"filtre_combo"));
 	while (i < 6)
 	{
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo),
-				combo_text[i]);
+		combo_text[i]);
 		i++;
 	}
 	gtk_combo_box_set_active(GTK_COMBO_BOX(combo), 0);
@@ -22,9 +22,9 @@ void	init_gtk_filter_widget(void)
 
 void	init_gtk_create_widget(void)
 {
-	GtkWidget *combo;
-	static char *combo_text[] = {"Sphere", "Plan", "Cone", "Cylindre", "Torus", "ADN"};
-	int		i;
+	GtkWidget	*combo;
+	static char	*combo_text[] = {"Sphere", "Plan", "Cone", "Cylindre", "Torus", "ADN"};
+	int			i;
 
 	i = 0;
 	combo = GTK_WIDGET(gtk_builder_get_object(g_env.build,"create_type"));
@@ -36,7 +36,7 @@ void	init_gtk_create_widget(void)
 	}
 }
 
-t_obj *add_object_type(char *s, t_obj *obj)
+t_obj	*add_object_type(char *s, t_obj *obj)
 {
 	if (!s)
 		obj->type = NONE;
@@ -55,9 +55,9 @@ t_obj *add_object_type(char *s, t_obj *obj)
 	return (obj);
 }
 
-t_obj *add_object_name(t_obj *obj)
+t_obj	*add_object_name(t_obj *obj)
 {
-	GtkEntry *name;
+	GtkEntry	*name;
 
 	name = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_name"));
 	if (ft_strcmp("", gtk_entry_get_text(name)) == 0)
@@ -66,11 +66,11 @@ t_obj *add_object_name(t_obj *obj)
 	return (obj);
 }
 
-t_obj *add_object_pos(t_obj *obj)
+t_obj	*add_object_pos(t_obj *obj)
 {
-	GtkEntry *posx;
-	GtkEntry *posy;
-	GtkEntry *posz;
+	GtkEntry	*posx;
+	GtkEntry	*posy;
+	GtkEntry	*posz;
 
 	posx = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_posx"));
 	posy = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_posy"));
