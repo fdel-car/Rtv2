d942871085cc72be8abbf1909c092f0c834415d9
victor <vic@MacBook-Pro-de-victor.local>
2016-12-13 19:40:46 +0100

    gtk_f9 norme

--- src/gtk/gtk_f9.c
+++ src/gtk/gtk_f9.c
@@ -4,35 +4,35 @@ void	save_translation_mesh_object2(t_obj *list, t_gtkdata *data, t_vect oldpos)
 {
 	if (ft_strcmp(data->desc,"posx") == 0)
 	{
-		list->v0.x  += (((t_obj*)data->obj)->pos.x - oldpos.x);
-		list->v1.x  += (((t_obj*)data->obj)->pos.x - oldpos.x);
-		list->v2.x  += (((t_obj*)data->obj)->pos.x - oldpos.x);
+		list->v0.x  += (((t_obj *)data->obj)->pos.x - oldpos.x);
+		list->v1.x  += (((t_obj *)data->obj)->pos.x - oldpos.x);
+		list->v2.x  += (((t_obj *)data->obj)->pos.x - oldpos.x);
 	}
 	else if (ft_strcmp(data->desc,"posy") == 0)
 	{
-		list->v0.y  += (((t_obj*)data->obj)->pos.y - oldpos.y);
-		list->v1.y  += (((t_obj*)data->obj)->pos.y - oldpos.y);
-		list->v2.y  += (((t_obj*)data->obj)->pos.y - oldpos.y);
+		list->v0.y  += (((t_obj *)data->obj)->pos.y - oldpos.y);
+		list->v1.y  += (((t_obj *)data->obj)->pos.y - oldpos.y);
+		list->v2.y  += (((t_obj *)data->obj)->pos.y - oldpos.y);
 	}
 	else if (ft_strcmp(data->desc,"posz") == 0)
 	{
-		list->v0.z += (((t_obj*)data->obj)->pos.z - oldpos.z);
-		list->v1.z += (((t_obj*)data->obj)->pos.z - oldpos.z);
-		list->v2.z += (((t_obj*)data->obj)->pos.z - oldpos.z);
+		list->v0.z += (((t_obj *)data->obj)->pos.z - oldpos.z);
+		list->v1.z += (((t_obj *)data->obj)->pos.z - oldpos.z);
+		list->v2.z += (((t_obj *)data->obj)->pos.z - oldpos.z);
 	}
 }
 
 void	save_translation_mesh_object3(t_obj *list, t_gtkdata *data, t_vect oldpos)
 {
-	if(ft_strcmp(data->desc,"posx") == 0)
+	if (ft_strcmp(data->desc,"posx") == 0)
 	{
 		list->pos.x  += (((t_obj *)data->obj)->pos.x - oldpos.x);
 	}
-	else if(ft_strcmp(data->desc,"posy") == 0)
+	else if (ft_strcmp(data->desc,"posy") == 0)
 	{
 		list->pos.y  += (((t_obj *)data->obj)->pos.y - oldpos.y);
 	}
-	else if(ft_strcmp(data->desc,"posz") == 0)
+	else if (ft_strcmp(data->desc,"posz") == 0)
 	{
 		list->pos.z += (((t_obj *)data->obj)->pos.z - oldpos.z);
 	}
@@ -47,7 +47,7 @@ void	save_translation_mesh_object(GtkEntry *entry, t_gtkdata *data, t_vect oldpo
 	ret = ft_atof(gtk_entry_get_text(entry));
 	while (list)
 	{
-		if(list->type == TRIANGLE)
+		if (list->type == TRIANGLE)
 			save_translation_mesh_object2(list, data, oldpos);
 		else
 			save_translation_mesh_object3(list, data, oldpos);
@@ -57,20 +57,20 @@ void	save_translation_mesh_object(GtkEntry *entry, t_gtkdata *data, t_vect oldpo
 
 void	save_entry_transformation_object2(t_gtkdata *data, float ret)
 {
-	if(ft_strcmp(data->desc,"posx") == 0)
+	if (ft_strcmp(data->desc,"posx") == 0)
 		((t_obj *)data->obj)->pos.x  = ret;
-	if(ft_strcmp(data->desc,"posy") == 0)
+	if (ft_strcmp(data->desc,"posy") == 0)
 		((t_obj *)data->obj)->pos.y  = ret;
-	if(ft_strcmp(data->desc,"posz") == 0)
+	if (ft_strcmp(data->desc,"posz") == 0)
 		((t_obj *)data->obj)->pos.z  = ret;
-	if(ft_strcmp(data->desc,"dirx") == 0)
+	if (ft_strcmp(data->desc,"dirx") == 0)
 		((t_obj *)data->obj)->dir.x  = ret;
-	if(ft_strcmp(data->desc,"diry") == 0)
+	if (ft_strcmp(data->desc,"diry") == 0)
 		((t_obj *)data->obj)->dir.y  = ret;
-	if(ft_strcmp(data->desc,"dirz") == 0)
+	if (ft_strcmp(data->desc,"dirz") == 0)
 		((t_obj *)data->obj)->dir.z  = ret;
 	((t_obj *)data->obj)->dir = vec_norm(((t_obj *)data->obj)->dir);
-	if(ft_strcmp(data->desc,"normx") == 0)
+	if (ft_strcmp(data->desc,"normx") == 0)
 		((t_obj *)data->obj)->norm.x  = ret;
 }
 
