6d50046adb444033232cf25f1985cccf45f9d2c5
Fabien DEL CARMEN <fdel-car@e1r12p6.42.fr>
2016-12-13 20:15:35 +0100

    Norme Fab

--- src/gtk/gtk_f11.c
+++ src/gtk/gtk_f11.c
@@ -1,4 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gtk_f11.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdel-car <fdel-car@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/13 20:10:44 by fdel-car          #+#    #+#             */
+/*   Updated: 2016/12/13 20:15:19 by fdel-car         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "rt.h"
+
+void	free_mater_text(t_mater *mat)
+{
+	free((mat->tex)->text);
+	free(mat->tex);
+	mat->tex = NULL;
+}
 
 void	select_text(GtkEntry *entry, t_gtkdata *data)
 {
@@ -19,25 +38,24 @@ void	select_text(GtkEntry *entry, t_gtkdata *data)
 	{
 		chooser = GTK_FILE_CHOOSER(dialog);
 		filename = gtk_file_chooser_get_filename(chooser);
-		//appeler le free_mater si besoin slo
-		//free(((t_obj *)data->obj)->mater.tex->text);
-		//free(((t_obj *)data->obj)->mater.tex);
+		if (((t_obj *)data->obj)->mater.tex)
+			free_mater_text(&(((t_obj *)data->obj)->mater));
 		((t_obj *)data->obj)->mater.tex = load_texture(filename);
 		g_free(filename);
 	}
 	launch_preview();
-	gtk_widget_destroy (dialog);
+	gtk_widget_destroy(dialog);
 }
 
 void	save_entry_material_light2(GtkEntry *entry, t_gtkdata *data,
 float *ret_f, int *ret_i)
 {
-	if (ft_strcmp(data->desc,"blur") == 0)
+	if (ft_strcmp(data->desc, "blur") == 0)
 	{
 		*ret_i = ft_atoi(gtk_entry_get_text(entry));
 		((t_light *)data->obj)->blur = *ret_f;
 	}
-	if (ft_strcmp(data->desc,"colr") == 0)
+	if (ft_strcmp(data->desc, "colr") == 0)
 	{
 		*ret_i = ft_atoi(gtk_entry_get_text(entry));
 		((t_light *)data->obj)->color.r = *ret_i;
@@ -49,18 +67,18 @@ void	save_entry_material_light(GtkEntry *entry, t_gtkdata *data)
 	float	ret_f;
 	int		ret_i;
 
-	if (ft_strcmp(data->desc,"intens") == 0)
+	if (ft_strcmp(data->desc, "intens") == 0)
 	{
 		ret_f = ft_atof(gtk_entry_get_text(entry));
 		((t_light *)data->obj)->intensity = ret_f;
 	}
 	save_entry_material_light2(entry, data, &ret_f, &ret_i);
-	if (ft_strcmp(data->desc,"colg") == 0)
+	if (ft_strcmp(data->desc, "colg") == 0)
 	{
 		ret_i = ft_atoi(gtk_entry_get_text(entry));
 		((t_light *)data->obj)->color.g = ret_i;
 	}
-	if (ft_strcmp(data->desc,"colb") == 0)
+	if (ft_strcmp(data->desc, "colb") == 0)
 	{
 		ret_i = ft_atoi(gtk_entry_get_text(entry));
 		((t_light *)data->obj)->color.b = ret_i;
