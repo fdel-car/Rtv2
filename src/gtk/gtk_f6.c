19855fcc6d112c051f3de5fa33c64009b1ba45b8
victor <vic@MacBook-Pro-de-victor.local>
2016-12-13 19:08:12 +0100

    gtk_f6 norme

--- src/gtk/gtk_f6.c
+++ src/gtk/gtk_f6.c
@@ -2,33 +2,33 @@
 
 void	clear_entry_3(void)
 {
-	GtkEntry *normx;
-	GtkEntry *normy;
-	GtkEntry *normz;
-	GtkEntry *alpha;
+	GtkEntry	*normx;
+	GtkEntry	*normy;
+	GtkEntry	*normz;
+	GtkEntry	*alpha;
 
 	alpha = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_angle"));
 	normx = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_normx"));
 	normy = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_normy"));
 	normz = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_normz"));
-	gtk_entry_set_text (normx,"");
-	gtk_entry_set_text (normy,"");
-	gtk_entry_set_text (normz,"");
-	gtk_entry_set_text (alpha,"");
+	gtk_entry_set_text(normx,"");
+	gtk_entry_set_text(normy,"");
+	gtk_entry_set_text(normz,"");
+	gtk_entry_set_text(alpha,"");
 }
 
 void	clear_entry_4(void)
 {
-	GtkEntry *shiny;
-	GtkEntry *refl;
-	GtkEntry *trans;
+	GtkEntry	*shiny;
+	GtkEntry	*refl;
+	GtkEntry	*trans;
 
 	trans = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_trans"));
 	refl = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_refl"));
 	shiny = GTK_ENTRY(gtk_builder_get_object(g_env.build,"e_shiny"));
-	gtk_entry_set_text (shiny,"");
-	gtk_entry_set_text (refl,"");
-	gtk_entry_set_text (trans,"");
+	gtk_entry_set_text(shiny,"");
+	gtk_entry_set_text(refl,"");
+	gtk_entry_set_text(trans,"");
 }
 
 void	clear_entry_widget_add_object(void)
@@ -45,12 +45,31 @@ void hide_window()
 					"popup_create_object")));
 }
 
+void	signal_add_object2(t_obj *obj, char *s)
+{
+	if (!s)
+		;
+	else if (ft_strcmp(s, "Torus") == 0)
+		add_torus_sphere(obj);
+	else if (ft_strcmp(s, "ADN") == 0)
+		add_adn(obj);
+	push_obj(obj);
+	clear_entry_widget_add_object();
+	gtk_widget_hide (GTK_WIDGET(gtk_builder_get_object(g_env.build,
+					"popup_create_object")));
+	create_list_of_objects();
+	launch_preview();
+	launch_thread();
+}
+
 void	signal_add_object(void)
 {
-	char *s = NULL;
-	t_obj *obj = NULL;
+	char	*s;
+	t_obj	*obj;
 
-	s = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(gtk_builder_get_object(g_env.build,"create_type")));
+	obj = 0;
+	s = gtk_combo_box_text_get_active_text(
+	GTK_COMBO_BOX_TEXT(gtk_builder_get_object(g_env.build,"create_type")));
 	obj = malloc(sizeof(t_obj));
 	init_obj(obj);
 	add_object_type(s,obj);
@@ -67,17 +86,5 @@ void	signal_add_object(void)
 	obj->next = NULL;
 	obj->mater.tex = NULL;
 	set_func(obj);
-	if (!s)
-		;
-	else if (ft_strcmp(s, "Torus") == 0)
-		add_torus_sphere(obj);
-	else if (ft_strcmp(s, "ADN") == 0)
-		add_adn(obj);
-	push_obj(obj);
-	clear_entry_widget_add_object();
-	gtk_widget_hide (GTK_WIDGET(gtk_builder_get_object(g_env.build,
-					"popup_create_object")));
-	create_list_of_objects();
-	launch_preview();
-	launch_thread();
+	signal_add_object2(obj, s);
 }
