622d6cdec954d83c2497902ea073c9b8d5b72868
Fabien DEL CARMEN <fdel-car@e1r12p6.42.fr>
2016-12-13 19:11:35 +0100

    Norme Fab

--- src/gtk/gtk_f2.c
+++ src/gtk/gtk_f2.c
@@ -1,25 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gtk_f2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdel-car <fdel-car@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/13 19:10:24 by fdel-car          #+#    #+#             */
+/*   Updated: 2016/12/13 19:11:20 by fdel-car         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "rt.h"
 
-void	check_filter()
+void	check_filter(void)
 {
 	char	*s;
 
 	s = gtk_combo_box_text_get_active_text(
-	GTK_COMBO_BOX_TEXT(gtk_builder_get_object(g_env.build,"filtre_combo")));
+	GTK_COMBO_BOX_TEXT(gtk_builder_get_object(g_env.build, "filtre_combo")));
 	if (!s)
-		return;
-	if (ft_strcmp("Sepia",s) == 0)
+		return ;
+	if (ft_strcmp("Sepia", s) == 0)
 		sepia_filter();
-	else if (ft_strcmp("Cartoon",s) == 0)
+	else if (ft_strcmp("Cartoon", s) == 0)
 		sobel_filter();
-	else if (ft_strcmp("Greyscale",s) == 0)
+	else if (ft_strcmp("Greyscale", s) == 0)
 		greyscale_filter();
 	free(s);
 }
 
 void	switch_1(char *s)
 {
-	if (ft_strcmp("Cartoon",s) == 0)
+	if (ft_strcmp("Cartoon", s) == 0)
 	{
 		g_env.oculus = FALSE;
 		g_env.oculus_left = FALSE;
@@ -29,7 +41,7 @@ void	switch_1(char *s)
 		init_limits();
 		sobel_filter();
 	}
-	if (ft_strcmp("Greyscale",s) == 0)
+	if (ft_strcmp("Greyscale", s) == 0)
 	{
 		g_env.oculus = FALSE;
 		g_env.oculus_left = FALSE;
@@ -43,7 +55,7 @@ void	switch_1(char *s)
 
 void	switch_2(char *s)
 {
-	if (ft_strcmp("Stereocopique",s) == 0)
+	if (ft_strcmp("Stereocopique", s) == 0)
 	{
 		g_env.stereo = TRUE;
 		g_env.stereo_red = TRUE;
@@ -54,7 +66,7 @@ void	switch_2(char *s)
 		g_env.scene.cam.x_ind = g_env.scene.cam.view_w / (float)WIDTH;
 		init_limits();
 	}
-	if (ft_strcmp("Oculus",s) == 0)
+	if (ft_strcmp("Oculus", s) == 0)
 	{
 		g_env.stereo = FALSE;
 		g_env.stereo_red = FALSE;
@@ -69,7 +81,7 @@ void	switch_2(char *s)
 
 void	switch_3(char *s)
 {
-	if (ft_strcmp("None",s) == 0)
+	if (ft_strcmp("None", s) == 0)
 	{
 		g_env.oculus = FALSE;
 		g_env.oculus_left = FALSE;
@@ -88,7 +100,7 @@ void	switch_filter(GtkWidget *combo)
 	char	*s;
 
 	s = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(combo));
-	if (ft_strcmp("Sepia",s) == 0)
+	if (ft_strcmp("Sepia", s) == 0)
 	{
 		g_env.oculus = FALSE;
 		g_env.oculus_left = FALSE;
